26796857770a6e3dc379c9321b154268ce6874f2
Julia Longtin <julial@turinglace.com>
2017-09-03 03:26:49 +0100

    spacing changes, use port 9001 in all environments, and use the proper form of quotes for static strings.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,22 +1,24 @@
-env = ENV['RAILS_ENV'] || "development"
+env = ENV['RAILS_ENV'] || 'development'
 
-
-worker_processes (env == "development" ? 1 : 4)
+worker_processes (env == 'development' ? 1 : 4)
 preload_app true
 timeout 30
 
-#if env != "development"
-#  pid "tmp/pids/unicorn.pid"
-#  stderr_path "log/unicorn_#{env}.log"
-#  stdout_path "log/unicorn_#{env}.log"
-#end
+# if env != "development"
+#   pid "tmp/pids/unicorn.pid"
+#   stderr_path "log/unicorn_#{env}.log"
+#   stdout_path "log/unicorn_#{env}.log"
+# end
 
-#if env == "development"
-  listen 9001
-#else
-#  listen "/tmp/.sock", :backlog => 2048
-#  listen 8080, :tcp_nopush => true
-#end
+# listen on the same port in all "environments".
+listen 9001
+
+# if env == "development"
+#  listen 9001
+# else
+#   listen "/tmp/.sock", :backlog => 2048
+#   listen 8080, :tcp_nopush => true
+# end
 
 before_fork do |server, worker|
   ##
@@ -30,9 +32,9 @@ before_fork do |server, worker|
   #
   # Using this method we get 0 downtime deploys.
   old_pid = File.join Rails.root, 'tmp/pids/unicorn.pid.oldbin'
-  if File.exists?(old_pid) && server.pid != old_pid
+  if File.exist?(old_pid) && server.pid != old_pid
     begin
-      Process.kill("QUIT", File.read(old_pid).to_i)
+      Process.kill('QUIT', File.read(old_pid).to_i)
     rescue Errno::ENOENT, Errno::ESRCH
       # someone else did our job for us
     end
