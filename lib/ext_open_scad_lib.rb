97c5e283df815b8f8788ac97c06082a0f962e3ba
Julia Longtin <julial@turinglace.com>
2017-09-03 03:34:45 +0100

    more comments, spacing changes, use the proper form of quotes for static strings, and remove some bad debugging code.

--- lib/ext_open_scad_lib.rb
+++ lib/ext_open_scad_lib.rb
@@ -1,56 +1,53 @@
 require 'fileutils'
 
+# manually call extopenscad.
 module ExtOpenScad
   def self.compile!(scad_script)
-    puts "moooo\n"*2
+    puts 'moooo\n' * 2
     puts scad_script
-    puts "\n"*3
+    puts '\n' * 3
 
     # create the temp files
-puts `whoami`    
-puts "thats done now"
-    FileUtils.mkpath(File.join Rails.root, "tmp", "scads")
-    files = Hash[ {input: ".scad", output: ".js"}.collect { |sym, ext| [sym, Tempfile.new(["loljk", ext], File.join(Rails.root, "tmp", "scads"))] } ]
+    FileUtils.mkpath(File.join(Rails.root, 'tmp', 'scads'))
+    # FIXME: use an actually random temp file name.
+    files = Hash[{ input: '.scad', output: '.js' }.collect { |sym, ext| [sym, Tempfile.new(['loljk', ext], File.join(Rails.root, 'tmp', 'scads'))] }]
     puts files.inspect
     stdin, stdout, stderr = [nil, nil, nil]
-    response_data = ""
-    puts "moo cows! LOL!"
+    response_data = ''
+    puts 'moo cows! LOL!'
 
     begin
-
-      puts "Begining the render!!"
-      # load the scad script in to the input file
+      puts 'Begining the render!!'
+      # load the scad script into the input file.
       files[:input].write(scad_script)
 
-# close the temp files immediately to save on file handles
-      files.each {|sym, f| f.close(false)}      
-#File.open(files[:input], 'w') {|f| f.write(scad_script) }
+      # close the temp files immediately to save on file handles.
+      files.each { |f| f.close(false) }
+      # File.open(files[:input], 'w') {|f| f.write(scad_script) }
 
-      puts "running extopenscad..."
-
-puts `whoami`
+      puts 'running extopenscad...'
 
       # process the input with the extopenscad js exporter
-      hs_lib = File.expand_path( File.join File.dirname(__FILE__), "hs", "ImplicitExportJS.hs" )
+      hs_lib = File.expand_path(File.join(File.dirname(__FILE__), 'hs', 'ImplicitExportJS.hs'))
       stdin, stdout, stderr, wait_thr = Open3.popen3("runhaskell #{hs_lib} #{files[:input].path} #{files[:output].path}")
 
       wait_thr.value # wait for extopenscad to return
-      puts "extopenscad complete!"
+      puts 'extopenscad complete!'
 
       # respond with the output file
       puts files[:output].path
-      response_data = File.open( files[:output].path ).read
+      response_data = File.open(files[:output].path).read
     rescue(e)
-      puts "Rendering Error"
+      puts 'Rendering Error'
       puts e.backtrace
     ensure
       # kill the temp files with fire!
-      files.each {|sym, f| f.unlink}
-      #recording the streams
-      response = { :format => "THREE.Geometry", :data => response_data, :stdout => (stdout.read || ""), :stderr => (stderr.read || "") }
+      files.each { |f| f.unlink }
+      # recording the streams
+      response = { format: 'THREE.Geometry', data: response_data, stdout: (stdout.read || ''), stderr: (stderr.read || '') }
       if response[:data].blank?
         response[:stderr] = response[:stdout] + response[:stderr]
-        response[:stdout] = ""
+        response[:stdout] = ''
       end
       puts response[:stdout]
       puts response[:stderr].red
