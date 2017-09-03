dfefcb0915230b46207afdd5bf2b91c12f5aac72
Julia Longtin <julial@turinglace.com>
2017-09-03 06:10:28 +0100

    use the proper form of quotes for static strings, and add missing ().

--- script/unicorn_server.rb
+++ script/unicorn_server.rb
@@ -1,17 +1,17 @@
-env = ENV['RAILS_ENV'] || "development"
+env = ENV['RAILS_ENV'] || 'development'
 capitalized_env = "#{env[0].upcase}#{env[1..-1]}"
-Dir.chdir(File.join File.dirname(__FILE__), "..")
+Dir.chdir(File.join(File.dirname(__FILE__), '..'))
 
-puts "\n#{capitalized_env} Rails Server: Running Setup\n#{"-"*50}"
+puts "\n#{capitalized_env} Rails Server: Running Setup\n#{'-' * 50}"
 
 `rm ./public/index.html`
 
-if env == "production"
-  puts "Precompiling.."
+if env == 'production'
+  puts 'Precompiling..'
   `bundle exec rake assets:precompile`
-  puts "[ DONE ]"
+  puts '[ DONE ]'
 end
 
-puts "\n#{capitalized_env} Rails Server: Running\n#{"-"*50}"
+puts "\n#{capitalized_env} Rails Server: Running\n#{'-' * 50}"
 
 exec 'unicorn -c ./config/unicorn.rb'
