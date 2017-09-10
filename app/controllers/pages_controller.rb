83bd3c3144ecd61fdde5c5058485681f029cf0e8
Julia Longtin <julial@turinglace.com>
2017-09-10 18:43:32 +0100

    add new url retreval function, support ssl for retrieval (without verification), remove editor from the cached pages list, and add the controller for the install page.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,13 @@
+I_KNOW_THAT_OPENSSL_VERIFY_PEER_EQUALS_VERIFY_NONE_IS_WRONG = nil
+
 # MVC dispatch for each page of the site.
 class PagesController < ApplicationController
   include ApplicationHelper
+  require 'openssl'
+  require 'mechanize'
+  OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
-  caches_page :index, :editor, :docs, :api, :tutorial, :faq, :examples
+  caches_page :index, :docs, :api, :tutorial, :faq, :examples, :install
 
   def index
     @editor = false
@@ -10,9 +15,19 @@ class PagesController < ApplicationController
 
   def editor
     @editor = true
-    @example = params['example'] || 'twisted_extrusion'
-    @example = @example.downcase.gsub('[^a-z0-9\-]', '').gsub('[- ]', '_').to_sym
-    raise 'example does not exist' unless example_exists?(@example)
+    if params['url']
+      agent=Mechanize.new
+      urlstr = 'http://' + params['url']
+      page = agent.get urlstr
+      code = page.content
+      @code = ('//pulled from ' + urlstr + "\n" + code).to_s.force_encoding('UTF-8').html_safe
+      @example = nil
+    else
+      @code = nil
+      @example = params['example'] || 'twisted_extrusion'
+      @example = @example.downcase.gsub('[^a-z0-9\-]', '').gsub('[- ]', '_').to_sym
+      raise 'example does not exist' unless example_exists?(@example)
+    end
   end
 
   def docs
@@ -32,6 +47,10 @@ class PagesController < ApplicationController
   end
 
   def examples
+    @editor = false
+  end
+
+  def install
     @editor = false
   end
 
